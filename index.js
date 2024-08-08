673298abbdbd0940085f0a26904eaf33111f62ea
tqtrang67 <tqtrang67@gmail.com>
2024-08-08 10:37:39 +0700

    fix scroll header change

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 document.getElementById("switchButton").onclick = function () {
 	document.getElementById("myBody").classList.toggle("dark");
 };
+
+$(window).on("scroll", function () {
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 80) {
+		$("#site-header").addClass("nav-fixed");
+	} else {
+		$("#site-header").removeClass("nav-fixed");
+	}
+});
+
+//Main navigation Active Class Add Remove
+$(".navbar-toggler").on("click", function () {
+	$("header").toggleClass("active");
+});
+$(document).on("ready", function () {
+	if ($(window).width() > 991) {
+		$("header").removeClass("active");
+	}
+	$(window).on("resize", function () {
+		if ($(window).width() > 991) {
+			$("header").removeClass("active");
+		}
+	});
+});
